741eacda33782ef03a5803e3ecc247f3f3396cfa
segler-alex <segler_alex@web.de>
2016-07-23 12:59:34 +0200

    tables work

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ angular.module('RadioBrowserApp', ["ui.router", "ui.bootstrap", "ui.bootstrap-sl
       url: "/",
       templateUrl: "partials/start.html"
     })
+    .state('countries', {
+      url: "/countries",
+      templateUrl: "partials/countries.html"
+    })
+    .state('languages', {
+      url: "/languages",
+      templateUrl: "partials/languages.html"
+    })
+    .state('tags', {
+      url: "/tags",
+      templateUrl: "partials/tags.html"
+    })
+    .state('codecs', {
+      url: "/codecs",
+      templateUrl: "partials/codecs.html"
+    })
     .state('list', {
       url: "/list",
       templateUrl: "partials/list.html"
