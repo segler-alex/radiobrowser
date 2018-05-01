ebb51db08692fad213297a13476dab26b3b4450a
Unknown <segler_alex@web.de>
2018-05-01 22:13:18 +0200

    scale video

--- controllers/modal_video.js
+++ controllers/modal_video.js
@@ -1,9 +1,10 @@
 angular.module('RadioBrowserApp').controller('ModalVideoInstanceCtrl', function ($scope, $uibModalInstance, video) {
-    console.log("PARAM:" + JSON.stringify(video));
+    console.log("VIDEO:" + JSON.stringify(video));
     $scope.result = video;
     
     var player = new Clappr.Player({parentId: "#player_clappr"});
     player.load(video, 'application/vnd.apple.mpegurl', true);
+    player.resize({height: 360, width: 640})
 
     $scope.ok = function () {
         $uibModalInstance.close();
