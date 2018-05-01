fd43d8f8ecdded0cf84b7ea096a3587c04cef410
Unknown <segler_alex@web.de>
2018-05-01 22:15:54 +0200

    fix

--- controllers/modal_video.js
+++ controllers/modal_video.js
@@ -2,9 +2,9 @@ angular.module('RadioBrowserApp').controller('ModalVideoInstanceCtrl', function 
     console.log("VIDEO:" + JSON.stringify(video));
     $scope.result = video;
     
-    var player = new Clappr.Player({parentId: "#player_clappr"});
-    player.load(video, 'application/vnd.apple.mpegurl', true);
-    player.resize({height: 360, width: 640})
+    var player = new Clappr.Player({parentId: "#player_clappr", autoPlay: true, source: video});
+    //player.load(video, 'application/vnd.apple.mpegurl', true);
+    //player.resize({height: 360, width: 640});
 
     $scope.ok = function () {
         $uibModalInstance.close();
