73472971eeea33186ae4a7787538277307afedc3
Unknown <segler_alex@web.de>
2018-05-01 22:44:41 +0200

    fix

--- controllers/modal_video.js
+++ controllers/modal_video.js
@@ -1,12 +1,16 @@
 angular.module('RadioBrowserApp').controller('ModalVideoInstanceCtrl', function ($scope, $uibModalInstance, video) {
     console.log("VIDEO:" + JSON.stringify(video));
     $scope.result = video;
-    
-    var player = new Clappr.Player({parentId: "#player_clappr", autoPlay: true, source: video});
-    //player.load(video, 'application/vnd.apple.mpegurl', true);
-    //player.resize({height: 360, width: 640});
+    var player = null;
+
+    function play(){
+        player = new Clappr.Player({parentId: "#player_clappr", autoPlay: true, source: video});
+        //player.resize({height: 360, width: 640});
+    }
 
     $scope.ok = function () {
         $uibModalInstance.close();
     };
+
+    play();
 });
